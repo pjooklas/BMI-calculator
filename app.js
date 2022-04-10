c59f8909f8dd6a1a23f49386ac0a40f902bf8a3f
pjooklas <pjooklas@users.noreply.github.com>
2022-04-10 14:59:04 +0300

    result color depending on value

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const queryElements = {
     weight: document.querySelector('#weight'),
     height: document.querySelector('#height'),
     btn: document.querySelector('button'),
-    result: document.querySelector('.result-input'),
+    result: document.querySelector('.result-output'),
     weightError: document.querySelector('.wrapper-error-weight'),
-    heightError: document.querySelector('.wrapper-error-height')
+    heightError: document.querySelector('.wrapper-error-height'),
+    resultColor: document.querySelector('.result-output')
 }
 
 queryElements.btn.addEventListener('click', () => {
     let isError = false;
+    let bmiColor = 'green';
 
     queryElements.result.innerHTML = '';
     queryElements.weightError.innerHTML = '';
@@ -28,5 +30,9 @@ queryElements.btn.addEventListener('click', () => {
     if (!isError) {
         const bmi = (queryElements.weight.value / queryElements.height.value / queryElements.height.value * 10000).toFixed(2);
         queryElements.result.innerHTML = bmi;
+        bmiColor = (bmi < 18.5) ? 'orange' :
+            (bmi > 24.9) ? 'red' :
+            'green';
+        queryElements.resultColor.style.color = bmiColor;
     }
 })
