59025de6622488a399ac840f4f4a66d6ed73aafb
pjooklas <pjooklas@users.noreply.github.com>
2022-04-10 14:34:47 +0300

    input validation

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
+import { IsValid } from "./validation/Isvalid.js";
+
 const queryElements = {
     weight: document.querySelector('#weight'),
     height: document.querySelector('#height'),
     btn: document.querySelector('button'),
-    result: document.querySelector('.result-input')
+    result: document.querySelector('.result-input'),
+    weightError: document.querySelector('.wrapper-error-weight'),
+    heightError: document.querySelector('.wrapper-error-height')
 }
 
 queryElements.btn.addEventListener('click', () => {
+    let isError = false;
+
     queryElements.result.innerHTML = '';
-    const bmi = (queryElements.weight.value / queryElements.height.value / queryElements.height.value * 10000).toFixed(2);
-    queryElements.result.innerHTML = bmi;
+    queryElements.weightError.innerHTML = '';
+    queryElements.heightError.innerHTML = '';
+
+    if (IsValid.weight(queryElements.weight.value)[0]) {
+        queryElements.weightError.innerHTML = IsValid.weight(queryElements.weight.value)[1];
+        isError = true;
+    }
+    if (IsValid.height(queryElements.height.value)[0]) {
+        queryElements.heightError.innerHTML = IsValid.height(queryElements.height.value)[1];
+        isError = true;
+    }
+
+    if (!isError) {
+        const bmi = (queryElements.weight.value / queryElements.height.value / queryElements.height.value * 10000).toFixed(2);
+        queryElements.result.innerHTML = bmi;
+    }
 })
